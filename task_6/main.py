a3c828b2f659e12c607ae17f6a162425ec9bc511
danchik <danikshved31@gmail.com>
2025-10-18 21:23:17 +0300

    уя

--- task_6/main.py
+++ task_6/main.py
@@ -1,1 +1,6 @@
-
+with open('text.txt', 'r', encoding = 'utf - 8') as a:
+    lines = a.readlines()
+    new_lines = lines.replace("а", "о")
+with open('output.txt', 'w', encoding="utf - 8") as b:
+    b.writelines(new_lines)
+print("файл создан")
